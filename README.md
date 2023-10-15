a9eb7e80e910c8204f3f45cffe165fdaf7cce16e
obasoro <obasorokunle@gmail.com>
2023-10-14 21:11:43 -0500

    working on k8s daily

---
+++ README.md
@@ -0,0 +1,25 @@
+#### 100 days Kubernetes
+
+#### Text Books and materials used
+
+- [] Kubernetes Up and Running
+
+- [] 50 Kubernetes Concept Every DevOps engineer should Know
+
+- [] Kubernetes Best Practices
+
+- [] Cloud Native Devops with Kubernetest
+I would be sharing details base on Books used as as a Branch
+
+Let's try out Terraform
+Install Terraform
+Initialise cloud provider
+Run "terraform init"
+Set-up a security group
+Make sure that you set up the file
+Try whether or not it works with "terraform plan" . Here we would expect something to add but 
+not necessarily something to change or to destroy.
+Don't display your credentials openly, you would probably want to use something like Ansible 
+vault or similar to store your credentials, do not store in plain text
+Once we have finished setting up our resources, we want to destroy them "terraform destroy"
+
---
+++ terraform.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+variable "host" {
+  type = string
+}
+
+variable "client_certificate" {
+  type = string
+}
+
+variable "client_key" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+provider "kubernetes" {
+  host = var.host
+
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+}
+
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "nginx"
+  }
+}
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "MyTestApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyTestApp"
+        }
+      }
+      spec {
+        container {
+          image = "nginx"
+          name  = "nginx-container"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.test.spec.0.template.0.metadata.0.labels.app
+    }
+    type = "NodePort"
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+  }
+}
+
